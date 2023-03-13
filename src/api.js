cd020d002ba2c9e9775692eb32687654e56db4a7
quitq-kozocom <quitq@kozo-japan.com>
2023-03-13 09:58:04 +0700

    update link api

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 export const fetchResponse = async (chat) => {
   try {
-    const response = await fetch('http://localhost:3080', {
+    const response = await fetch('https://server-chatgpt-beta.vercel.app', {
       method: 'POST',
       headers: {
         "Content-Type": "application/json"
